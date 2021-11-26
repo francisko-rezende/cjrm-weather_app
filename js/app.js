d0c881a708e1b8a3b1f23d60ba9a531df3dde726
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2021-11-26 10:26:52 -0300

    Implement weather icon and city img features

--- js/app.js
+++ js/app.js
@@ -1,12 +1,35 @@
+const cityNameContainer = document.querySelector('[data-js="city-name"]')
+const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
+const cityTemperatureContainer =  document.querySelector('[data-js="city-temperature"]')
+const cityCard = document.querySelector('[data-js="city-card"]')
 const cityForm = document.querySelector('[data-js="change-location"]')
+const timeIconConContainer = document.querySelector('[data-js="time-icon"]')
+let timeImg = document.querySelector('[data-js="time"]')
 
 cityForm.addEventListener('submit', async event => {
   event.preventDefault()
 
   const inputValue = event.target.city.value
-  const [{Key, LocalizedName}] = await getCityData(inputValue)
-  const [{weatherText, Temperature}] = await getCityWeather(Key)
+  const [{ Key, LocalizedName }] = await getCityData(inputValue)
+  const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = 
+    await getCityWeather(Key)
+  const timeIcon = `<img src="../src/icons/${WeatherIcon}.svg" />`
 
-  console.log(weatherText, Temperature)
+  if (cityCard.classList.contains('d-none')) {
+    cityCard.classList.remove('d-none')
+  }
+
+  if (IsDayTime) {
+    timeImg.src = '../src/day.svg'
+  } else {
+    timeImg.src = '../src/night.svg'
+  }
+
+  timeIconConContainer.innerHTML = timeIcon
+  cityNameContainer.textContent = `${LocalizedName}`
+  cityWeatherContainer.textContent = `${WeatherText}`
+  cityTemperatureContainer.textContent = `${Temperature.Metric.Value}`
+
+  // console.log(WeatherText, Temperature.Metric.Value)
   event.target.reset()
 })
